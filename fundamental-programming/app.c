4c22381c3afbfa648d4b82afaead9bc34b546160
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2022-01-03 06:10:03 +0700

    Menambahkan file fundamental-programming

---
+++ fundamental-programming/app.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    //Program Menghitung Luas
+    printf("Program Menghitung Luas\n");
+    int p, l;
+    printf("Masukkan Panjang : ");
+    scanf("%d", &p);
+    printf("Masukkan Lebar : ");
+    scanf("%d", &l);
+    printf("Luas Persegi Panjang = %d\n", p * l);
+    return 0;
+}
