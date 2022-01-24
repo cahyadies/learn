bb895c9f2dd1ebe405735919685a24fdfb7a7539
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2022-01-24 22:17:22 +0700

    Komentar

---
+++ belajar-python/komentar/main.py
@@ -0,0 +1,25 @@
+# Ini adalah komentar
+# Tulisan ini tidak akan dieksekusi
+
+# Komentar dengan tanda pagar hanya bisa digunakan
+# Untuk
+# Satu
+# Baris
+
+"""
+Penulisan komentar lebih dari satu baris yaitu 
+dengan menggunakan kutip dua 3 kali dan 
+ditutup dengan kutip dua 3 kalu juga
+"""
+
+print("Hello World")  # ini juga komentar
+
+# Print ("Welcome")
+
+# Komentar bisa berisi spesial karakter !@#$%^&*(),./;'[]\
+
+# Mencetak nama
+print("Budi")
+
+# Mencetak angka/integer
+print(123)
